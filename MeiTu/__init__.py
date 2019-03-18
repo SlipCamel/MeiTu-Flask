1428868dac775303eb32bc785b94674430e22489
GaiFan <343633128@qq.com>
2019-03-18 19:22:24 +0800

    update 0.1

--- MeiTu/__init__.py
+++ MeiTu/__init__.py
@@ -9,10 +9,12 @@ def create_app(config_name=None):
     if config_name is None:
         config_name = os.getenv('FLASK_CONFIG', 'development')
     app = Flask('MeiTu')
+    app.jinja_env.trim_blocks = True
+    app.jinja_env.lstrip_blocks = True
     app.config.from_object(config[config_name])
 
     @app.route('/')
     def hello():
-        return render_template('base.html')
+        return render_template('MeiTu/index.html')
 
     return app
